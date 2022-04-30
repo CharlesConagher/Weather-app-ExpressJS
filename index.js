aafd41a641ed6838fabc845f3989bc2c2d074115
CharlesConagher <mihail.romashenko03@mail.ru>
2022-04-30 17:51:40 +0300

    minor changes before deployment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
 
-const PORT = process.env.PORT ?? 3000
+const PORT = process.env.PORT || 3000
 const __dirname = path.resolve()
 app.use('/public', express.static(__dirname + '/public'))
 app.use(express.json({limit: '1mb' }));
